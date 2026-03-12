1200861c87274c25c5c26f7a05df02cb4acf51b3
frdnu <fardeenusman391@gmail.com>
2026-03-12 22:19:08 +0500

    some changes were left

--- main.py
+++ main.py
@@ -4,20 +4,16 @@ from utils.parser import Parser
 
 memory = MemoryManager()
 
-# 1. Simulate user input
 user_input = "Remind me to Fix the CSS at high priority due tomorrow"
 
-# 2. Parse the input into a dictionary
 data = Parser.parse_command(user_input)
 
-# 3. Create the object dynamically
 new_task = Task(
     title=data['title'],
     priority=data['priority'],
     due_date=data['due_date']
 )
 
-# 4. Save to database
 memory.add_task(new_task)
 
 print(
